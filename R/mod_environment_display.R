c0b38c60fd12d6ca925b44bdd2871568ecb303b3
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-06-11 15:01:36 -0400

    env modal text to blurb div theme_minimal for climate trends

--- R/mod_environment_display.R
+++ R/mod_environment_display.R
@@ -32,11 +32,15 @@ mod_environment_display_server <- function(id, sites, region,  lookup_vec){
       plot_one_site(site_clicked = region(), site_df = sites,  lookup_vec = lookup_vec)
     )
     
-    output$blurb = renderText("Les différents sites de surveillance du MFFP connaissent des précipitations et des températures différentes. 
+    output$blurb <- renderUI({
+      tags$div(id = "blurbid", class = "blurbtext",
+               tags$h2(paste0("Projections climatiques pour ", region())),
+               tags$p("Les différents sites de surveillance du MFFP connaissent des précipitations et des températures différentes. 
 Chaque cercle ci-dessous montre la variation annuelle de la pluie et de la température pour un seul site. Le site sur lequel vous avez cliqué est représenté par une ligne plus épaisse.")
+               )})
     
-    output$rain = ggiraph::renderGirafe(plot_to_show()$precip)
-    output$heat = ggiraph::renderGirafe(plot_to_show()$temper)
+    output$rain <- ggiraph::renderGirafe(plot_to_show()$precip)
+    output$heat <- ggiraph::renderGirafe(plot_to_show()$temper)
     
     
   })
