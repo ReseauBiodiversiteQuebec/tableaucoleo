19fd34e2a3ee11dec50c21c25eb0d8c460644c2f
Guillaume Larocque <glaroc@gmail.com>
2022-02-17 11:19:56 -0500

    Sunburst!

--- R/mod_sunburst.R
+++ R/mod_sunburst.R
@@ -24,32 +24,18 @@ mod_sunburst_server <- function(id, species_data){
     output$sunburst_plot<-plotly::renderPlotly({
       print(species_data)
       ns <- session$ns
-      names <- unique(species_data[!grepl('|',species_data$taxa_name, fixed = TRUE),'taxa_name'])
-      taxo <- taxize::gnr_resolve(names$taxa_name,fields='all',data_source_ids = c(1),best_match_only = TRUE)
-      long_hier <- taxo |> tidyr::separate_rows(classification_path,sep="\\|")
-      sun_data <- long_hier |> 
-        dplyr::group_by(classification_path) |> 
-        dplyr::summarise(n=dplyr::n())
-      for (i in 1:nrow(sun_data)){
-        s <- as.character(sun_data[i,'classification_path'])
-        sp <- long_hier |> dplyr::filter(classification_path==s) |> dplyr::select(submitted_name)
-        sp <- as.character(sp[1,1])
-        h <- taxo |> dplyr::filter(submitted_name==sp) |> dplyr::select(classification_path) |> as.character() |> strsplit(split='\\|')
-        ind <- which(h[[1]]==s)[1]
-        if(ind==1){
-          sun_data[i,'parent'] <- ""
-        }else{
-          sun_data[i,'parent'] <- h[[1]][ind-1]
-        }
-      }
-      sun_data<-sun_data[!sun_data$classification_path=='Biota',]
-      sun_data[sun_data$parent=='Biota','parent'] <- ''
-      plotly::plot_ly(
-        labels = sun_data$classification_path,
-        parents = sun_data$parent,
-        type = 'sunburst',
-        maxdepth = 5
-      )
+      sun_data=sunburst_create_data()
+      plotly::plot_ly() |>
+        plotly::add_trace(
+          labels = sun_data$classification_path,
+          parents = sun_data$parent,
+          type = 'sunburst',
+          maxdepth = 5
+        ) |>
+        plotly::layout(
+          sunburstcolorway = c("#538887", "#7bb5b1"),
+          extendsunburstcolors = TRUE
+        )
     })
   })
 }
---
+++ R/sunburst_data.R
@@ -0,0 +1,35 @@
+#' Sunburst data
+#'
+#' @param species_data 
+#'
+#' @return
+#' @export
+sunburst_create_data <- function(species_data){
+  f<-file.info('sun_data.RDS')
+  if((Sys.time()-f$mtime) > 1440){
+    names <- unique(species_data[!grepl('|',species_data$taxa_name, fixed = TRUE),'taxa_name'])
+    taxo <- taxize::gnr_resolve(names$taxa_name,fields='all',data_source_ids = c(1),best_match_only = TRUE)
+    long_hier <- taxo |> tidyr::separate_rows(classification_path,sep="\\|")
+    sun_data <- long_hier |> 
+      dplyr::group_by(classification_path) |> 
+      dplyr::summarise(n=dplyr::n())
+    for (i in 1:nrow(sun_data)){
+      s <- as.character(sun_data[i,'classification_path'])
+      sp <- long_hier |> dplyr::filter(classification_path==s) |> dplyr::select(submitted_name)
+      sp <- as.character(sp[1,1])
+      h <- taxo |> dplyr::filter(submitted_name==sp) |> dplyr::select(classification_path) |> as.character() |> strsplit(split='\\|')
+      ind <- which(h[[1]]==s)[1]
+      if(ind==1){
+        sun_data[i,'parent'] <- ""
+      }else{
+        sun_data[i,'parent'] <- h[[1]][ind-1]
+      }
+    }
+    sun_data<-sun_data[!sun_data$classification_path=='Biota',]
+    sun_data[sun_data$parent=='Biota','parent'] <- ''
+    saveRDS(sun_data,'sun_data.RDS')
+  }else{
+    sun_data<-readRDS('sun_data.RDS')
+  }
+  return(sun_data)
+}
